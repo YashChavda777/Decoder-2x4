6a0118dec1d25a23c4e9cfaf3c09593e98fc5dc9
YashChavda777 <140133564+YashChavda777@users.noreply.github.com>
2023-09-13 22:15:54 +0530

    Create Decoder 2x4.v

---
+++ Decoder 2x4.v
@@ -0,0 +1,12 @@
+module Decoder2x4 (
+    input A,
+    input B,
+    output [3:0] Y
+);
+
+assign Y[0] = (~A) & (~B);
+assign Y[1] = (~A) & B;
+assign Y[2] = A & (~B);
+assign Y[3] = A & B;
+
+endmodule
